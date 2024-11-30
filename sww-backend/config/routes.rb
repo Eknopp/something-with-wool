53ff0858303a938872c4b2be4bcaf8fda1ee7a20
Eitan Knopp <eknop93@gmail.com>
2024-11-30 19:24:51 +0100

    feat: user authentication

--- sww-backend/config/routes.rb
+++ sww-backend/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resource :session, only: [:create, :destroy] # Login and logout
+      resources :passwords, only: [:create, :update], param: :token # Password reset flow
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
