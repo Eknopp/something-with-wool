3d3b844cd20ce8060bdeef959ee7baa7194dcbb8
Eitan Knopp <eknop93@gmail.com>
2024-12-01 17:44:41 +0100

    small cleanup

--- sww-frontend/src/App.tsx
+++ sww-frontend/src/App.tsx
@@ -1,10 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return <h1 className="text-4x1 text-blue-500 mt-10">Vite Project</h1>;
 }
 
