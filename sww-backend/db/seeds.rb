c81f5e3102635878aa2395c3eba3e0c702f013d5
Eitan Knopp <eknop93@gmail.com>
2025-01-04 18:44:43 +0100

    chore: db seed config

--- sww-backend/db/seeds.rb
+++ sww-backend/db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Enable logging of SQL statements
+ActiveRecord::Base.logger = Logger.new($stdout)
+
+def seed(model, find_or_create_by, update_with = {})
+  record = model.where(find_or_create_by).first_or_initialize
+
+  if record.update(update_with)
+    record
+  else
+    raise "Couldn't save #{record.class} (#{record.errors.full_messages.join(", ")})"
+  end
+end
+
+if !Rails.env.development?
+  Rails.logger.info "Seeding disabled - not in development mode"
+  return
+end
+ 
+seed User, {email: 'local@test.com'},{password:'123456'}
