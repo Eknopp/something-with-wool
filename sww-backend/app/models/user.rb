67975c874718a2c5f894e41bcce099ed11b42be8
Eitan Knopp <eknop93@gmail.com>
2025-02-01 16:36:10 +0100

    chore: annotations and linter setup

--- sww-backend/app/models/user.rb
+++ sww-backend/app/models/user.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  jti                    :string           not null
+#  refresh_token          :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_jti                   (jti) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
+#
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
@@ -8,14 +31,14 @@ class User < ApplicationRecord
     jwt_revocation_strategy: self
 
   def refresh_token_valid?
-    return false unless refresh_token.present?
+    return false if refresh_token.blank?
 
     refresh_token_secret_key = SecretKeysConfiguration::REFRESH_TOKEN_SECRET
     decoded_token = decode_refresh_token(refresh_token, refresh_token_secret_key)
     return false unless decoded_token
 
     exp = decoded_token["exp"]
-    Time.at(exp) > Time.now
+    Time.zone.at(exp) > Time.zone.now
   end
 
   private
