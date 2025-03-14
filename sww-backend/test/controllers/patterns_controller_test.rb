5e611ab9b6adaad79c0d655b35c501c120d0d01d
Eitan Knopp <eknop93@gmail.com>
2025-03-14 13:14:56 +0100

    WIP chore: test controllers pattern and project

---
+++ sww-backend/test/controllers/patterns_controller_test.rb
@@ -0,0 +1,49 @@
+require "test_helper"
+
+class PatternsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  setup do
+    # TODO: might need to catch cookies when signing in and add to requests
+    @user = users(:one)
+    sign_in @user
+    @pattern_params = {
+      name: "Test Pattern",
+      difficulty_rating: 3,
+      user_id: @user.id,
+      craft_type: "knitting",
+      currency: "USD",
+      first_published_date: Date.today,
+      gauge_pattern: "10x10",
+      languages: ["en"],
+      likes: 0,
+      nbr_rows: 10,
+      nbr_stitches: 10,
+      notes: "Test notes",
+      overall_rating: 4,
+      overall_yarn_weight: "fine",
+      pdf_path: "path/to/pdf",
+      pictures_path: "path/to/pictures",
+      price: 10.0,
+      release_date: Date.today,
+      repeats: 2,
+      sizes: ["M"],
+      swatch_size: 5,
+      total_yardage: 100
+    }
+  end
+
+  test "should create pattern" do
+    assert_difference("Pattern.count") do
+      post v1_patterns_url, params: {pattern: @pattern_params}
+    end
+    assert_response :success
+  end
+
+  test "should not create pattern with invalid data" do
+    @pattern_params[:name] = nil
+    assert_no_difference("Pattern.count") do
+      post v1_patterns_url, params: {pattern: @pattern_params}
+    end
+    assert_response :unprocessable_entity
+  end
+end
